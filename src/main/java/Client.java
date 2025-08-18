0aee68d2b792410b8851b6c5e5e5b5b70b0dcf5b
BRYCEN-VN\lp_phat <lp_phat@brycen.com.vn>
2025-08-18 09:12:36 +0700

    Expiry

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -9,7 +9,7 @@ public class Client {
         Socket clientSocket = new Socket("127.0.0.1", 6379);
 
         OutputStream outputStream = clientSocket.getOutputStream();
-        outputStream.write("PING hey".getBytes());
+        outputStream.write("*5\r\n$3\r\nSET\r\n$9\r\nblueberry\r\n$5\r\napple\r\n$2\r\npx\r\n$3\r\n100\r\n".getBytes());
 
 
         BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
