bc1fccd8bcce62ac4e0109bb015f4326f62badf4
le-pp2402 <lephiphatphat@gmail.com>
2025-08-25 02:58:37 +0700

    fix

--- src/main/java/solver/impl/XRange.java
+++ src/main/java/solver/impl/XRange.java
@@ -5,10 +5,7 @@ import constants.ID;
 import container.Container;
 import solver.ICommandHandler;
 import solver.Pair;
-
-import javax.xml.crypto.Data;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 public class XRange implements ICommandHandler {
@@ -22,36 +19,30 @@ public class XRange implements ICommandHandler {
         }
         System.out.println("********************");;
 
-        for (var e : Container.container.entrySet()) {
+        for (var e: Container.streamContainer.entrySet()) {
+            ID id = ID.parse(e.getKey());
+            ID left = ID.parse(args.get(1));
+            ID right = ID.parse(args.get(2));
 
-            System.out.println(e.getKey());
-            System.out.println(e.getValue());
+            assert left != null && right != null && id != null;
+            if (!inRange(left, right, id)) continue;
 
-            if (e.getKey().equals(args.get(0)) && Container.streamContainer.contains(e.getValue())) {
-                ID id = ID.parse(e.getValue());
-                ID left = ID.parse(args.get(0));
-                ID right = ID.parse(args.get(1));
-
-                assert left != null && right != null && id != null;
-                if (!inRange(left, right, id)) continue;
-
-                var props = Container.streamContainer.get(id);
-                var allProps = new ArrayList<String>();
-                for (var elem : props.entrySet()) {
-                    allProps.add(elem.getKey());
-                    allProps.add(elem.getValue());
-                }
-
-                StringBuilder sb = new StringBuilder();
-                sb.append(DataType.BULK_STRING);
-                sb.append(e.getKey().length());
-                sb.append("\r\n");
-                sb.append(e.getValue());
-                sb.append("\r\n");
-
-                sb.append(toRESP(allProps));
-                res.add(sb.toString());
+            var props = Container.streamContainer.get(id.toString());
+            var allProps = new ArrayList<String>();
+            for (var elem : props.entrySet()) {
+                allProps.add(elem.getKey());
+                allProps.add(elem.getValue());
             }
+
+            StringBuilder sb = new StringBuilder();
+            sb.append(DataType.BULK_STRING.getSymbol());
+            sb.append(e.getKey().length());
+            sb.append("\r\n");
+            sb.append(e.getValue());
+            sb.append("\r\n");
+
+            sb.append(toRESP(allProps));
+            res.add(sb.toString());
         }
 
         StringBuilder sb = new StringBuilder();
@@ -66,7 +57,7 @@ public class XRange implements ICommandHandler {
     }
 
     public boolean inRange(ID left, ID right, ID value) {
-        return  (left.compareTo(value) >= 0 && right.compareTo(value) <= 0);
+        return  (left.compareTo(value) <= 0 && right.compareTo(value) >= 0);
     }
 
     public String toRESP(List<String> args) {
